f2a6c2265df7bc122d9bb143b81a4f28940a2e46
Даниил Куксарь <dan.kukar@yandex.ru>
2025-03-02 18:04:53 +0300

    fixed: serializers fixed

--- telemetry/collector/src/main/java/ru/yandex/practicum/sensor/kafka/SensorEventAvroSerializer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/sensor/kafka/SensorEventAvroSerializer.java
@@ -1,39 +1,16 @@
 package ru.yandex.practicum.sensor.kafka;
 
-import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.DatumWriter;
-import org.apache.avro.io.EncoderFactory;
-import org.apache.avro.specific.SpecificDatumWriter;
-import org.apache.kafka.common.errors.SerializationException;
-import org.apache.kafka.common.serialization.Serializer;
+import ru.yandex.practicum.config.BaseAvroSerializer;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-public class SensorEventAvroSerializer implements Serializer<SensorEventAvro> {
-    private final EncoderFactory encoderFactory = EncoderFactory.get();
-    private BinaryEncoder encoder;
-    private static final DatumWriter<SensorEventAvro> WRITER =
-            new SpecificDatumWriter<>(SensorEventAvro.class);
-
+public class SensorEventAvroSerializer extends BaseAvroSerializer<SensorEventAvro> {
+    public SensorEventAvroSerializer(DatumWriter<SensorEventAvro> writer) {
+        super(writer);
+    }
 
     @Override
     public byte[] serialize(String topic, SensorEventAvro data) {
-        try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-            if (data == null) return null;
-
-            byte[] result;
-
-            encoder = encoderFactory.binaryEncoder(out, encoder);
-
-            WRITER.write(data, encoder);
-            encoder.flush();
-
-            result = out.toByteArray();
-            return result;
-        } catch (IOException ex) {
-            throw new SerializationException("Data serialization error for topic [" + topic + "]", ex);
-        }
+        return super.serialize(topic, data);
     }
 }
