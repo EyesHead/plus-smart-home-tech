4b3fea2160e6c5dfa64cd53e7cc73443899b5318
Даниил Куксарь <dan.kukar@yandex.ru>
2025-05-19 21:24:17 +0300

    debug: Добавлены логи для отладки SnapshotProcessor

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/AnalyzerRunner.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/AnalyzerRunner.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.analyzer;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.analyzer.event.HubEventProcessor;
@@ -11,20 +12,25 @@ import ru.yandex.practicum.analyzer.snapshot.SnapshotProcessor;
 @Component
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 @RequiredArgsConstructor
+@Slf4j
 public class AnalyzerRunner implements CommandLineRunner {
     HubEventProcessor hubEventProcessor;
     SnapshotProcessor snapshotProcessor;
 
     @Override
-    public void run(String... args) throws Exception {
-        // запускаем в отдельном потоке обработчик событий
-        // от пользовательских хабов
+    public void run(String... args) {
+        log.info("AnalyzerRunner запускается");
+
         Thread hubEventsThread = new Thread(hubEventProcessor);
         hubEventsThread.setName("HubEventHandlerThread");
         hubEventsThread.start();
+        log.info("HubEventProcessor запущен в отдельном потоке");
 
-        // В текущем потоке начинаем обработку
-        // снимков состояния датчиков
+        // ❗ Важно: snapshotProcessor.start() блокирует поток, убедись, что это ожидаемо
+        log.info("Запускаю SnapshotProcessor в текущем потоке (будет заблокирован)");
         snapshotProcessor.start();
+
+        // Этот лог никогда не дойдёт, так как start() — бесконечный цикл
+        log.info("Этот лог не должен быть достигнут — snapshotProcessor.start() бесконечен");
     }
 }
